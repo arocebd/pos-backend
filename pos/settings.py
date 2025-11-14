71d4ea2a6c6d0a25ff14b65bf5313129f9165bf8
arocebd <arocebd@gmail.com>
2025-11-14 21:58:07 +0600

    Update settings.py

--- pos/settings.py
+++ pos/settings.py
@@ -9,7 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'your-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [    
+    "localhost",
+    "127.0.0.1",
+    "pos-backend-0cog.onrender.com",]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
