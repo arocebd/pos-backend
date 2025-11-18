33b60215bb79e2ab9c0f59546d8ca12567666906
arocebd <arocebd@gmail.com>
2025-11-19 01:44:38 +0600

    Update settings.py

--- pos/settings.py
+++ pos/settings.py
@@ -45,6 +45,11 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+CSRF_TRUSTED_ORIGINS = env.list(
+    "CSRF_TRUSTED_ORIGINS",
+    default=[]
+)
+
 ROOT_URLCONF = "pos.urls"
 
 TEMPLATES = [
